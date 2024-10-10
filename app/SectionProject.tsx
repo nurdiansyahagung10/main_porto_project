ad15dc712350fd28f31c053e3e029498146843e9
nurdiansyahagung385 <nurdiansyahagung385@gmail.com>
2024-10-10 06:08:38 +0000

    deploy

--- app/SectionProject.tsx
+++ app/SectionProject.tsx
@@ -1,157 +1,159 @@
 import Image from "next/image";
 
 const SectionProject = () => {
-    return (
-        <>
-        <div className="w-full mt-20 mb-10 flex justify-center">
-            <button className="py-1.5 items-center flex gap-1 bg-white  border text-purple-600 shadow  font-medium rounded-xl px-4 " >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-              </svg>
-              Selected work
+  return (
+    <>
+      <div className="w-full mt-20 mb-10 flex justify-center">
+        <button className="py-1.5 items-center flex gap-1 bg-white border text-purple-600 shadow font-medium rounded-xl px-4">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2.5" stroke="currentColor" className="size-4">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+          </svg>
+          Selected work
+        </button>
+      </div>
+
+      <div className="w-full flex flex-col mb-10 items-center text-center">
+        <h1 className="font-semibold lg:text-6xl md:text-5xl text-4xl mb-10">Pixel Design Work</h1>
+
+        <p className="text-stone-600 pe-22 md:w-[50%]">
+          Let's sneak a peek at the project where I've transformed it into a new solution with my design approach.
+        </p>      </div>
+
+      <div className="grid md:grid-cols-2 grid-cols-1">
+        <div className="border-r border-b border-dashed md:p-7">
+          <div className="border md:p-3 p-2 rounded-3xl">
+            <div className="bg-white w-full h-auto">
+              <Image
+                src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
+                alt=""
+                className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+
+
+          <div className="mt-7 flex items-center justify-between">
+            <h1 className="text-lg font-medium">Website sistem</h1>
+            <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
+              <div className="flex gap-2 items-center">
+                <span>
+                  Go there
+                </span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+                </span>
+              </div>
+
             </button>
-          </div>
-
-          <div className="w-full flex flex-col mb-10 items-center text-center">
-            <h1 className="font-semibold lg:text-6xl md:text-5xl text-4xl mb-10">Pixel Design Work</h1>
-            <p className="text-stone-600 pe-22 md:w-[50%]">Lets sneak a peek at the project where Ive transformed it into a new solution with my design approach.</p>
-          </div>
-
-          <div className="grid md:grid-cols-2 grid-cols-1">
-            <div className="border-r border-b border-dashed md:p-7">
-              <div className="border md:p-3 p-2 rounded-3xl">
-                <div className="bg-white w-full h-auto">
-                  <Image
-                    src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
-                    alt=""
-                    className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-
-
-              <div className="mt-7 flex items-center justify-between">
-                <h1 className="text-lg font-medium">Website sistem</h1>
-                <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
-                  <div className="flex gap-2 items-center">
-                    <span>
-                      Go there
-                    </span>
-                    <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                    </span>
-                  </div>
-
-                </button>
-
-              </div>
-              <div className="mt-4">
-                <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
-              </div>
-            </div>
-            <div className=" md:border-l-0 border-l border-b border-dashed md:p-7">
-              <div className="border md:p-3 p-2 rounded-3xl">
-                <Image
-                  src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
-                  alt=""
-                  className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
-                  width={100}
-                  height={100}
-                />
-
-              </div>
-              <div className="mt-7 flex items-center justify-between">
-                <h1 className="text-lg font-medium">Website sistem</h1>
-                <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
-                  <div className="flex gap-2 items-center">
-                    <span>
-                      Go there
-                    </span>
-                    <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                    </span>
-                  </div>
-
-                </button>
-
-              </div>
-              <div className="mt-4">
-                <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
-              </div>
-            </div>
-            <div className="border-r  border-dashed md:p-7">
-              <div className="border md:p-3 p-2 rounded-3xl">
-                <div className="bg-white w-full h-auto">
-                  <Image
-                    src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
-                    alt=""
-                    className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
-                    width={100}
-                    height={100}
-                  />
-                </div>
-              </div>
-
-
-              <div className="mt-7 flex items-center justify-between">
-                <h1 className="text-lg font-medium">Website sistem</h1>
-                <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
-                  <div className="flex gap-2 items-center">
-                    <span>
-                      Go there
-                    </span>
-                    <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                    </span>
-                  </div>
-
-                </button>
-
-              </div>
-              <div className="mt-4">
-                <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
-              </div>
-            </div>
-            <div className=" md:border-l-0 border-l  border-dashed md:p-7">
-              <div className="border md:p-3 p-2 rounded-3xl">
-                <Image
-                  src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
-                  alt=""
-                  className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
-                  width={100}
-                  height={100}
-                />
-
-              </div>
-              <div className="mt-7 flex items-center justify-between">
-                <h1 className="text-lg font-medium">Website sistem</h1>
-                <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
-                  <div className="flex gap-2 items-center">
-                    <span>
-                      Go there
-                    </span>
-                    <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
-                    </svg>
-                    </span>
-                  </div>
-
-                </button>
-
-              </div>
-              <div className="mt-4">
-                <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
-              </div>
-            </div>
 
           </div>
-        </>
-    );
+          <div className="mt-4">
+            <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
+          </div>
+        </div>
+        <div className=" md:border-l-0 border-l border-b border-dashed md:p-7">
+          <div className="border md:p-3 p-2 rounded-3xl">
+            <Image
+              src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
+              alt=""
+              className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
+              width={100}
+              height={100}
+            />
+
+          </div>
+          <div className="mt-7 flex items-center justify-between">
+            <h1 className="text-lg font-medium">Website sistem</h1>
+            <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
+              <div className="flex gap-2 items-center">
+                <span>
+                  Go there
+                </span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+                </span>
+              </div>
+
+            </button>
+
+          </div>
+          <div className="mt-4">
+            <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
+          </div>
+        </div>
+        <div className="border-r  border-dashed md:p-7">
+          <div className="border md:p-3 p-2 rounded-3xl">
+            <div className="bg-white w-full h-auto">
+              <Image
+                src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
+                alt=""
+                className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+
+
+          <div className="mt-7 flex items-center justify-between">
+            <h1 className="text-lg font-medium">Website sistem</h1>
+            <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
+              <div className="flex gap-2 items-center">
+                <span>
+                  Go there
+                </span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+                </span>
+              </div>
+
+            </button>
+
+          </div>
+          <div className="mt-4">
+            <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
+          </div>
+        </div>
+        <div className=" md:border-l-0 border-l  border-dashed md:p-7">
+          <div className="border md:p-3 p-2 rounded-3xl">
+            <Image
+              src={'/header/profile/b1oysJMBAcwj6oXLrF6f191BmU.avif'}
+              alt=""
+              className="aspect-video shadow-lg rounded-3xl w-full h-full object-cover"
+              width={100}
+              height={100}
+            />
+
+          </div>
+          <div className="mt-7 flex items-center justify-between">
+            <h1 className="text-lg font-medium">Website sistem</h1>
+            <button className="py-1.5 items-center flex gap-1 bg-white  border   font-medium rounded-xl px-4 " >
+              <div className="flex gap-2 items-center">
+                <span>
+                  Go there
+                </span>
+                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="size-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                </svg>
+                </span>
+              </div>
+
+            </button>
+
+          </div>
+          <div className="mt-4">
+            <p className="text-stone-600 md:w-[70%]">We design Websites and Landing pages to meet any business solution and challenge.</p>
+          </div>
+        </div>
+
+      </div>
+    </>
+  );
 }
 
 export default SectionProject;
